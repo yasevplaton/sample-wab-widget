fa2ccf116a9c34e0e9fd4b95397fc9360a5415a7
Platon Yasev <yasevplaton@gmail.com>
2022-07-06 22:54:40 +0300

    Move UI to separate file

--- Widget.js
+++ Widget.js
@@ -18,10 +18,6 @@ define(["dojo/_base/declare", "jimu/BaseWidget"], function (
   BaseWidget
 ) {
   var clazz = declare([BaseWidget], {
-    templateString:
-      "<div>This is a very simple widget. " +
-      '<input type="button" value="Get Map Id" data-dojo-attach-event="click:_getMapId">.</div>',
-
     _getMapId: function () {
       alert(this.map.id);
     },
